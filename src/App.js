082174ab286ab0bab447578a25a44bef35fa82b5
Roger Humphreys <rhumfloripa@gmail.com>
2018-12-12 10:27:16 -0200

    Add book e implementando (get update) (Prateleira e estante)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react'
 import { Route } from 'react-router-dom';
-import * as BooksAPI from './BooksAPI'
+// import * as BooksAPI from './BooksAPI'
 import ListBooks from './ListBooks';
 import SearchBooks from './SearchBooks';
 import './App.css';
 class BooksApp extends React.Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
-  }
+
 
   render() {
       return (
           <div className="app">
-              <Route exact path="/" component={ListBooks}/>
+              <Route exact path="/" component={ListBooks} />
               <Route path="/search" component={SearchBooks}/>
           </div>
       );
---
+++ src/Book.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+class Book extends Component {
+    handleChange = event => {
+        this.props.onShelfChange(this.props.book, event.target.value);
+    };
+    render() {
+        return (
+            <li>
+                <div className="book">
+                    <div className="book-top">
+                        <div
+                            className="book-cover"
+                            style={{
+                                width: 128,
+                                height: 193,
+                                backgroundImage: `url(${this.props.book.imageLinks.thumbnail})`
+                            }}
+                        />
+                        <div className="book-shelf-changer">
+                            <select
+                                value={this.props.book.shelf}
+                                onChange={this.handleChange}
+                            >
+                                <option value="none" disabled>
+                                    Move to...
+                                </option>
+                                <option value="currentlyReading">Currently Reading</option>
+                                <option value="wantToRead">Want to Read</option>
+                                <option value="read">Read</option>
+                                <option value="none">None</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div className="book-title">
+                        {this.props.book.title}
+                    </div>
+                    <div className="book-authors">
+                        {this.props.book.authors.join(', ')}
+                    </div>
+                </div>
+            </li>
+        );
+    }
+}
+export default Book;
